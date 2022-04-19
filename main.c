f42718aa012f6b352817b00420b29bf4deccddea
Blessingdev233 <blessing.lartey@gmail.com>
2022-04-19 12:04:00 +0000

    main

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdio.h>
+#include "mainn.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
