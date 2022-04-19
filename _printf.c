5f6336248702c4d3bea196ce560cd02e97a89e08
Blessingdev233 <blessing.lartey@gmail.com>
2022-04-19 13:33:50 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -1,49 +1,52 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-
+#include <stddef.h>
 /**
- *_printf - Print a formatted string
- *@format: format string
- *Return: number of characters printed
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list list;
-	char *pointer, *start;
-	param_func flags = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-	va_start(list, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (pointer = (char *)format; *pointer; pointer++)
+	if (format != NULL)
 	{
-		init_params(&flags, list);
-		if (*pointer != '%')
-		{
-			count += _putchar(*pointer);
-			continue;
-		}
-		start = pointer;
-		pointer++;
-		while (get_flags(pointer, &flags))
-		{
-			pointer++;
-		}
-		pointer = get_width(pointer, &flags, list);
-		pointer = get_precision(pointer, &flags, list);
-		if (get_mods(pointer, &flags))
-			pointer++;
+		int count = 0, i;
+		int (*m)(va_list);
+		va_list args;
 
-		if (!func_parse(pointer))
-			count += print_range(start, pointer,
-			flags.l_mod || flags.h_mod ? pointer - 1 : 0);
-		else
-			count += print_func(pointer, list, &flags);
+		va_start(args, format);
+		i = 0;
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+		while (format != NULL && format[i] != '\0')
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i += 2;
+				}
+				else
+				{
+					m = get_func(format[i + 1]);
+					if (m)
+						count += m(args);
+					else
+						count = _putchar(format[i]) + _putchar(format[i + 1]);
+					i += 2;
+				}
+			}
+			else
+			{
+				count += _putchar(format[i]);
+				i++;
+			}
+		}
+		va_end(args);
+		return (count);
 	}
-	_putchar(-1);
-	va_end(list);
-	return (count);
+	return (-1);
 }
