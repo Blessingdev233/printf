7fb0fc15090720594181bec98cc0958e94671c82
princeardayfio <princeardayfio@yahoo.com>
2022-04-19 20:39:13 +0000

    updatiing putchar

--- _putchar.c
+++ _putchar.c
@@ -7,34 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(int c)
+int _putchar(char c)
 {
-	static char buf[1024];
-	static int pos;
-
-	if (c == -1 || pos >= 1024)
-	{
-		write(1, buf, pos);
-		pos = 0;
-	}
-	if (c != -1)
-	{
-		buf[pos++] = c;
-	}
-	return (1);
-}
-
-/**
- * _puts - prints a string with newline
- * @str: the string to print
- *
- * Return: number of characters printed
- */
-int _puts(char *str)
-{
-	char *count = str;
-
-	while (*str)
-		_putchar(*str++);
-	return (str - count);
+	return (write(1, &c, 1));
 }
