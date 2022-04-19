83a243e4e408bbccbe811262c0183c7563de5eba
Blessingdev233 <blessing.lartey@gmail.com>
2022-04-19 11:51:27 +0000

    header file 1.0

--- main.h
+++ main.h
@@ -17,3 +17,20 @@ typedef struct structprint
 	char *q;
 	int (*u)(char *format, va_list);
 } structype;
+
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
+#endif
